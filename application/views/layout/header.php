353ca793643541c37eaa550d9ccc03512139bc34
andripery <32670571+andripery@users.noreply.github.com>
2019-08-06 21:31:46 +0700

    Web

---
+++ application/views/layout/header.php
@@ -0,0 +1,106 @@
+<!doctype html>
+<html lang="zxx">
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <title>shotgear</title>
+    <link rel="icon" href="<?php echo base_url();?>assets/img/favicon.png">
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="<?php echo base_url();?>assets/css/bootstrap.min.css">
+    <!-- animate CSS -->
+    <link rel="stylesheet" href="<?php echo base_url();?>assets/css/animate.css">
+    <!-- owl carousel CSS -->
+    <link rel="stylesheet" href="<?php echo base_url();?>assets/css/owl.carousel.min.css">
+    <!-- font awesome CSS -->
+    <link rel="stylesheet" href="<?php echo base_url();?>assets/css/all.css">
+    <!-- flaticon CSS -->
+    <link rel="stylesheet" href="<?php echo base_url();?>assets/css/flaticon.css">
+    <link rel="stylesheet" href="<?php echo base_url();?>assets/css/themify-icons.css">
+    <link rel="stylesheet" href="<?php echo base_url();?>assets/css/swiper.min.css">
+    <!-- font awesome CSS -->
+    <link rel="stylesheet" href="<?php echo base_url();?>assets/css/magnific-popup.css">
+    <!-- swiper CSS -->
+    <link rel="stylesheet" href="<?php echo base_url();?>assets/css/slick.css">
+    <!-- style CSS -->
+    <link rel="stylesheet" href="<?php echo base_url();?>assets/css/style.css">
+</head>
+
+<body>
+    <!--::header part start::-->
+    <header class="main_menu home_menu">
+        <div class="container">
+            <div class="row align-items-center">
+                <div class="col-lg-12">
+                    <nav class="navbar navbar-expand-lg navbar-light">
+                        <a class="navbar-brand" href="<?php echo base_url();?>assets/index.html"> <img src="<?php echo base_url();?>assets/img/logo.png" alt="logo"> </a>
+                        <button class="navbar-toggler" type="button" data-toggle="collapse"
+                            data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
+                            aria-expanded="false" aria-label="Toggle navigation">
+                            <span class="menu_icon"><i class="fas fa-bars"></i></span>
+                        </button>
+
+                        <div class="collapse navbar-collapse main-menu-item" id="navbarSupportedContent">
+                            <ul class="navbar-nav">
+                                <li class="nav-item">
+                                    <a class="nav-link" href="<?php echo base_url();?>assets/index.html">Home</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link" href="<?php echo base_url();?>assets/about.html">about</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link" href="<?php echo base_url();?>assets/services.html">services</a>
+                                </li>
+                                <li class="nav-item dropdown">
+                                    <a class="nav-link dropdown-toggle" href="<?php echo base_url();?>assets/" id="navbarDropdownpro"
+                                        role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                        portfolio
+                                    </a>
+                                    <div class="dropdown-menu" aria-labelledby="navbarDropdownpro">
+                                        <a class="dropdown-item" href="<?php echo base_url();?>assets/portfolio.html">portfolio</a>
+                                        <a class="dropdown-item" href="<?php echo base_url();?>assets/portfolio_details.html">portfolio details</a>
+                                    </div>
+                                </li>
+
+                                <li class="nav-item dropdown">
+                                    <a class="nav-link dropdown-toggle" href="<?php echo base_url();?>assets/blog.html" id="navbarDropdown"
+                                        role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                        Blog
+                                    </a>
+                                    <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                                        <a class="dropdown-item" href="<?php echo base_url();?>assets/blog.html"> blog</a>
+                                        <a class="dropdown-item" href="<?php echo base_url();?>assets/single-blog.html">Single blog</a>
+                                    </div>
+                                </li>
+                                <li class="nav-item dropdown">
+                                    <a class="nav-link dropdown-toggle" href="<?php echo base_url();?>assets/blog.html" id="navbarDropdown1"
+                                        role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                        pages
+                                    </a>
+                                    <div class="dropdown-menu" aria-labelledby="navbarDropdown1">
+                                        <a class="dropdown-item" href="<?php echo base_url();?>assets/elements.html">Elements</a>
+                                    </div>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link" href="<?php echo base_url();?>assets/contact.html">contact</a>
+                                </li>
+                            </ul>
+                        </div>
+                        <div class="dropdown cart">
+                            <a class="dropdown-toggle" href="<?php echo base_url();?>assets/#" id="navbarDropdown3" role="button"
+                                data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                <i class="flaticon-bag"></i>
+                            </a>
+                            <!-- <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                                    <div class="single_product">
+    
+                                    </div>
+                                </div> -->
+                        </div>
+                    </nav>
+                </div>
+            </div>
+        </div>
+    </header>
+    <!-- Header part end-->
