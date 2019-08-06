9838b51e353474e7d5a465546c8b2e0aa96c6099
andripery <32670571+andripery@users.noreply.github.com>
2019-08-07 04:00:19 +0700

    Admin

--- application/views/admin.php
+++ application/views/admin.php
@@ -16,9 +16,9 @@
     <meta name="description" content="CoreUI - Open Source Bootstrap Admin Template">
     <meta name="author" content="Łukasz Holeczek">
     <meta name="keyword" content="Bootstrap,Admin,Template,Open,Source,jQuery,CSS,HTML,RWD,Dashboard">
-    <title>CoreUI Free Bootstrap Admin Template</title>
+    <title>Elaeis </title>
     <!-- Icons-->
-    <link rel="icon" type="image/ico" href="<?php echo base_url();?>assets/admin/./img/favicon.ico" sizes="any" />
+    <link rel="icon" type="image/ico" href="<?php echo base_url();?>assets/img/favicon1.png" sizes="any" />
     <link href="<?php echo base_url();?>assets/admin/node_modules/@coreui/icons/css/coreui-icons.min.css" rel="stylesheet">
     <link href="<?php echo base_url();?>assets/admin/node_modules/flag-icon-css/css/flag-icon.min.css" rel="stylesheet">
     <link href="<?php echo base_url();?>assets/admin/node_modules/font-awesome/css/font-awesome.min.css" rel="stylesheet">
@@ -46,14 +46,14 @@
       <button class="navbar-toggler sidebar-toggler d-lg-none mr-auto" type="button" data-toggle="sidebar-show">
         <span class="navbar-toggler-icon"></span>
       </button>
-      <a class="navbar-brand" href="<?php echo base_url();?>assets/admin/#">
-        <img class="navbar-brand-full" src="<?php echo base_url();?>assets/admin/img/brand/logo.svg" width="89" height="25" alt="CoreUI Logo">
-        <img class="navbar-brand-minimized" src="<?php echo base_url();?>assets/admin/img/brand/sygnet.svg" width="30" height="30" alt="CoreUI Logo">
+      <a class="navbar-brand" href="<?php echo site_url('welcome/admin');?>">
+        <img class="navbar-brand-full" src="<?php echo base_url();?>assets/img/logoo.png" width="120" height="25" alt="CoreUI Logo">
+        <img class="navbar-brand-minimized" src="<?php echo site_url('welcome/admin');?>assets/admin/img/brand/sygnet.svg" width="30" height="30" alt="CoreUI Logo">
       </a>
       <button class="navbar-toggler sidebar-toggler d-md-down-none" type="button" data-toggle="sidebar-lg-show">
         <span class="navbar-toggler-icon"></span>
       </button>
-      <ul class="nav navbar-nav d-md-down-none">
+      <!-- <ul class="nav navbar-nav d-md-down-none">
         <li class="nav-item px-3">
           <a class="nav-link" href="<?php echo base_url();?>assets/admin/#">Dashboard</a>
         </li>
@@ -63,24 +63,24 @@
         <li class="nav-item px-3">
           <a class="nav-link" href="<?php echo base_url();?>assets/admin/#">Settings</a>
         </li>
-      </ul>
+      </ul> -->
       <ul class="nav navbar-nav ml-auto">
-        <li class="nav-item d-md-down-none">
+        <!-- <li class="nav-item d-md-down-none">
           <a class="nav-link" href="<?php echo base_url();?>assets/admin/#">
             <i class="icon-bell"></i>
             <span class="badge badge-pill badge-danger">5</span>
           </a>
-        </li>
-        <li class="nav-item d-md-down-none">
+        </li> -->
+       <!--  <li class="nav-item d-md-down-none">
           <a class="nav-link" href="<?php echo base_url();?>assets/admin/#">
             <i class="icon-list"></i>
           </a>
-        </li>
-        <li class="nav-item d-md-down-none">
+        </li> -->
+        <!-- <li class="nav-item d-md-down-none">
           <a class="nav-link" href="<?php echo base_url();?>assets/admin/#">
             <i class="icon-location-pin"></i>
           </a>
-        </li>
+        </li> -->
         <li class="nav-item dropdown">
           <a class="nav-link" data-toggle="dropdown" href="<?php echo base_url();?>assets/admin/#" role="button" aria-haspopup="true" aria-expanded="false">
             <img class="img-avatar" src="<?php echo base_url();?>assets/admin/img/avatars/6.jpg" alt="admin@bootstrapmaster.com">
