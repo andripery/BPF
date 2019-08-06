4ba4993075065862e638f970675769204a2f94b7
Jefran Aprilliando <jefran17ti@mahasiswa.pcr.ac.id>
2019-08-07 05:01:48 +0700

    Update about.php

--- application/views/about.php
+++ application/views/about.php
@@ -1,18 +1,19 @@
 <?php $this->load->view('layout/header');?>
-    <!-- breadcrumb start-->
+<!--::banner part start::-->
     <section class="banner_part" style="background-image: url('<?php echo base_url();?>assets/img/banner_bg1.jpg');">
         <div class="container">
-            <div class="row">
-                <div class="col-lg-12">
-                    <div class="breadcrumb_iner text-center">
-                        <div class="breadcrumb_iner_item">
-                            <h2>about <span>us</span></h2>
+            <div class="row align-items-center justify-content-end">
+                <div class="col-lg-5">
+                    <div class="banner_text text-center">
+                        <div class="banner_text_iner">        
+                            <h1>About <span>Us</span></h1>
                         </div>
                     </div>
-                </div>
+                </div>  
             </div>
         </div>
     </section>
+    <!--::banner part start::-->
     <!-- breadcrumb start-->
 
     <!--::about_us part start::-->
