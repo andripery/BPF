7d013d79c0d13085f0891997e7239d8e95a148d3
Jefran Aprilliando <jefran17ti@mahasiswa.pcr.ac.id>
2019-08-07 01:15:01 +0700

    Update home.php

--- application/views/home.php
+++ application/views/home.php
@@ -25,9 +25,8 @@
             <div class="row align-items-center justify-content-center">
                 <div class="col-lg-8">
                     <div class="about_us_text text-center">
-                        <h5>About our studio</h5>
-                        <h2>The Camera Is An Instrument That Teaches People
-                            how to See without a Camera</h2>
+                        <h5>About our Web</h5>
+                        <h2>Struktur Perkebunan Kelapa Sawit Provinsi Riau</h2>
                         <p>It’s the end of summer the sweltering heat makes human
                             sweat in the night and makes the plants and trees wilt
                             even in the moonlit nights. The eastern wind breeze brings
