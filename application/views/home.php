ffd7872d72b950dc5454f4aa069d5ffa8fbc78c0
Jefran Aprilliando <jefran17ti@mahasiswa.pcr.ac.id>
2019-08-07 03:54:13 +0700

    Update home.php

--- application/views/home.php
+++ application/views/home.php
@@ -7,10 +7,10 @@
                 <div class="col-lg-5">
                     <div class="banner_text text-center">
                         <div class="banner_text_iner">
-                            <h5>Model Photography</h5>
-                            <h1>Creative <span>Studio</span></h1>
-                            <p>Capturing moments from today</p>
-                            <a href="<?php echo base_url();?>assets/#" class="btn_1">view work</a>
+                            <h5>Save money with</h5>
+                            <h1>Palm <span>oil</span></h1>
+                            <p>Belajar menghemat uang mulai sekarang</p>
+                            <a href="<?php echo base_url();?>"class="btn_1">view web</a>
                         </div>
                     </div>
                 </div>  
@@ -25,7 +25,6 @@
             <div class="row align-items-center justify-content-center">
                 <div class="col-lg-8">
                     <div class="about_us_text text-center">
-                        <h5>About our Web</h5>
                         <h2>Perkembangan Perkebunan Kelapa Sawit di Riau</h2>
                         <p>Perkembangan perkebunan kelapa sawit di Provinsi Riau memberikan peran penting
 dalam kesejahteraan masyarakatnya dan kemajuan daerah, dilihat dari profibilitas kelapa sawit
@@ -38,113 +37,13 @@ dan tahun 2009 sebesar 33,86%. Sedangkan kontribusi pertanian terhadap lapangan 
 masih cukup besar yaitu, pada tahun 1990 mencapai angka 58,13% dan pada tahun 2010
 mencapai angka 43,89%. Pada tabel 1.3 berikut dapat dlihat perkembangan luas perkebunan
 kelapa sawit Provinsi Riau dari tahun 1984-2009 (dalam Chalid, 2011) .</p>
-                        <a href="<?php echo base_url();?>assets/#" class="btn_2">read more</a>
+                        <a href="<?php echo base_url();?>" class="btn_2">read more</a>
                     </div>
                 </div>
             </div>
         </div>
     </section>
     <!--::about_us part end::-->
-
-    <!-- gallery_part part start-->
-    <section class="gallery_part section_padding">
-        <div class="container-fluid">
-            <div class="row">
-                <div class="col-lg-5 offset-lg-2">
-                    <div class="section_tittle">
-                        <p>recent project</p>
-                        <h2>Check latest work</h2>
-                    </div>
-                </div>
-            </div>
-            <div class="row">
-                <div class="col-lg-8 offset-lg-2">
-                    <div class="portfolio-filter filters">
-                        <ul>
-                            <li class="active" data-filter="all">Semua Foto</li>
-                            <li data-filter="1">Pengelolaan Sawit</li>
-                            <li data-filter="2">fashion</li>
-                            <li data-filter="3">portrait</li>
-                            <li data-filter="4">magazine</li>
-                        </ul>
-                    </div>
-                </div>
-            </div>
-            <div class="row">
-                <div class="col-xl-12">
-                    <div class="gallery_part_item filtr-container">
-                        <a href="<?php echo base_url();?>assets/portfolio_details.html" class="img-gal filtr-item" data-category="1"
-                            style="background-image: url('<?php echo base_url();?>assets/img/gallery/gallery_item_1.png')">
-                            <div class="single_gallery_item">
-                                <div class="single_gallery_item_iner">
-                                    <div class="gallery_item_text">
-                                        <p>Fashion photography</p>
-                                        <h4>Smiling beutiful women</h4>
-                                    </div>
-                                </div>
-                            </div>
-                        </a>
-                        <a href="<?php echo base_url();?>assets/portfolio_details.html" class="img-gal width-1 filtr-item" data-category="1"
-                            style="background-image: url('<?php echo base_url();?>assets/img/gallery/gallery_item_2.png')">
-                            <div class="single_gallery_item">
-                                <div class="single_gallery_item_iner">
-                                    <div class="gallery_item_text">
-                                        <p>Fashion photography</p>
-                                        <h4>Smiling beutiful women</h4>
-                                    </div>
-                                </div>
-                            </div>
-                        </a>
-                        <a href="<?php echo base_url();?>assets/portfolio_details.html" class="img-gal width-2 filtr-item" data-category="3"
-                            style="background-image: url('<?php echo base_url();?>assets/img/gallery/gallery_item_3.png')">
-                            <div class="single_gallery_item">
-                                <div class="single_gallery_item_iner">
-                                    <div class="gallery_item_text">
-                                        <p>Fashion photography</p>
-                                        <h4>Smiling beutiful women</h4>
-                                    </div>
-                                </div>
-                            </div>
-                        </a>
-                        <a href="<?php echo base_url();?>assets/portfolio_details.html" class="img-gal width-2 filtr-item" data-category="4"
-                            style="background-image: url('<?php echo base_url();?>assets/img/gallery/gallery_item_4.png')">
-                            <div class="single_gallery_item">
-                                <div class="single_gallery_item_iner">
-                                    <div class="gallery_item_text">
-                                        <p>Fashion photography</p>
-                                        <h4>Smiling beutiful women</h4>
-                                    </div>
-                                </div>
-                            </div>
-                        </a>
-                        <a href="<?php echo base_url();?>assets/portfolio_details.html" class="img-gal width-1 filtr-item" data-category="1"
-                            style="background-image: url('<?php echo base_url();?>assets/img/gallery/gallery_item_5.png')">
-                            <div class="single_gallery_item">
-                                <div class="single_gallery_item_iner">
-                                    <div class="gallery_item_text">
-                                        <p>Fashion photography</p>
-                                        <h4>Smiling beutiful women</h4>
-                                    </div>
-                                </div>
-                            </div>
-                        </a>
-                        <a href="<?php echo base_url();?>assets/portfolio_details.html" class="img-gal filtr-item" data-category="2"
-                            style="background-image: url('<?php echo base_url();?>assets/img/gallery/gallery_item_6.png')">
-                            <div class="single_gallery_item">
-                                <div class="single_gallery_item_iner">
-                                    <div class="gallery_item_text">
-                                        <p>Fashion photography</p>
-                                        <h4>Smiling beutiful women</h4>
-                                    </div>
-                                </div>
-                            </div>
-                        </a>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </section>
-    <!-- gallery_part part end-->
 
     <!--::our_service part start::-->
     <section class="our_service padding_bottom">
