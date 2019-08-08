810298f96c89b983f58da28342f54562fa4fb5bd
Jefran Aprilliando <jefran17ti@mahasiswa.pcr.ac.id>
2019-08-08 22:52:53 +0700

    n

--- application/views/about.php
+++ application/views/about.php
@@ -33,26 +33,10 @@ Untuk itu, pengenalan manajemen keuangan itu sendiri sangat dibutuhkan untuk par
         </div>
     </section>
     <!--::about_us part end::-->
-
-    <!-- intro_video_bg start-->
-    <section class="intro_video_bg">
-        <div class="container">
-            <div class="row">
-                <div class="col-lg-12">
-                    <div class="intro_video_iner text-center">
-                        <h2>Our project video</h2>
-                        <div class="intro_video_icon">
-                            <a id="play-video_1" class="video-play-button popup-youtube"
-                                href="https://www.youtube.com/watch?v=9-8utRYZ-vs">
-                                <span class="ti-control-play"></span>
-                            </a>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </section>
-    <!-- intro_video_bg part start-->
+    <div class="container">
+        <img src="<?php echo base_url();?>assets/img/kelapa.png">
+    </div>
+   
 
     <!--::review_part part start::-->
     <section class="review_part section_padding">
